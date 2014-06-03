857d1a9a17ce27e16fc287364e4caf3aad9c1221
Kristian Kanya <kriskanya@gmail.com>
2014-06-02 22:34:51 -0500

    initial

---
+++ app/models/user.js
@@ -0,0 +1,34 @@
+var bcrypt = require('bcrypt');
+var userCollection = global.nss.db.collection('users');
+
+class User{
+  static register(obj, fn){
+    userCollection.findOne({email:obj.email}, (e,u)=>{
+      if(!u){
+        var user = new User();
+        user.email = obj.email;
+        user.password = bcrypt.hashSync(obj.password, 8);
+        userCollection.save(user, ()=>fn(user));
+      }else{
+        fn(null);
+      }
+    });
+  }
+
+  static login(obj, fn){
+    userCollection.findOne({email:obj.email}, (e,u)=>{
+      if(u){
+        var isMatch = bcrypt.compareSync(obj.password, u.password);
+        if(isMatch){
+          fn(u);
+        }else{
+          fn(null);
+        }
+      }else{
+        fn(null);
+      }
+    });
+  }
+}
+
+module.exports = User;
---
+++ test/unit/user_spec.js
@@ -0,0 +1,76 @@
+/* global describe, it, before, beforeEach */
+/* jshint expr:true */
+
+'use strict';
+
+process.env.DBNAME = 'todo-test';
+
+var expect = require('chai').expect;
+var Mongo = require('mongodb');
+var app = require('../../app/app');
+var request = require('supertest');
+var traceur = require('traceur');
+
+var User;
+
+describe('User', function(){
+  before(function(done){
+    request(app)
+    .get('/')
+    .end(function(){
+      User = traceur.require(__dirname + '/../../app/models/user.js');
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    global.nss.db.collection('users').drop(function(){
+      User.register({email:'sue@aol.com', password:'abcd'}, function(){
+        done();
+      });
+    });
+  });
+
+  describe('.register', function(){
+    it('should successfully register a user', function(done){
+      User.register({email:'bob@aol.com', password:'1234'}, function(u){
+        expect(u).to.be.ok;
+        expect(u).to.be.an.instanceof(User);
+        expect(u._id).to.be.an.instanceof(Mongo.ObjectID);
+        expect(u.password).to.have.length(60);
+        done();
+      });
+    });
+
+    it('should NOT successfully register a user', function(done){
+      User.register({email:'sue@aol.com', password:'does not matter'}, function(u){
+        expect(u).to.be.null;
+        done();
+      });
+    });
+  });
+
+  describe('.login', function(){
+    it('should successfully login a user', function(done){
+      User.login({email:'sue@aol.com', password:'abcd'}, function(u){
+        expect(u).to.be.ok;
+        done();
+      });
+    });
+
+    it('should NOT login user - bad email', function(done){
+      User.login({email:'wrong@aol.com', password:'abcd'}, function(u){
+        expect(u).to.be.null;
+        done();
+      });
+    });
+
+    it('should NOT login user - bad password', function(done){
+      User.login({email:'sue@aol.com', password:'wrong'}, function(u){
+        expect(u).to.be.null;
+        done();
+      });
+    });
+  });
+
+});
